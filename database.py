4cb101dd8e50b119e41c9d58810fb039482413e0
josomm22 <josommefeld@icloud.com>
2020-01-07 17:05:25 +0200

    Initial commit

---
+++ database.py
@@ -0,0 +1,44 @@
+database = {
+    'students':{
+
+        }
+
+    }
+
+ron = {
+    'id': 1,
+    'first_name': 'ron',
+    'last_name': 'weasley',
+    'date_created': '07/01/2020',
+    'last_updated': '07/01/2020',
+    'existing_skillz': [[1,4],[3,2],[9,5],[14,4],[16,2]],
+    'desired_skillz': [[1,5],[2,5],[6,4],[14,5],[3,2]],
+    'course_interests': [3,4]
+}
+
+skillz = {
+    1:'alchemy',
+    2: 'animation',
+    3: 'conjuror',
+    4: 'disintegration',
+    5: 'elemental',
+    6: 'healing',
+    7: 'illusion',
+    8: 'immortality',
+    9: 'invulnerability',
+    10: 'necromancer',
+    11: 'omnipresent',
+    12: 'omniscient',
+    13: 'poison',
+    14: 'possession',
+    15: 'self-detonation',
+    16: 'summoning',
+    17: 'water breathing',
+
+}
+courses_list = {
+    1: 'alchemy basics',
+    2: 'alchemy advanced',
+    3: 'magic for day-to-day life',
+    4: 'dating with magic',
+}
