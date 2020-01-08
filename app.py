307f91b54aa26eafae04d2b15762d35256cd1b2f
josomm22 <josommefeld@icloud.com>
2020-01-08 19:02:49 +0200

    eh

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def update_student_details(student_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)    time.sleep(0.5)
+    app.run(host='0.0.0.0', port=port)
